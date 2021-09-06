769859d35dca4f435a86bd9e8f462b103ec568d6
Sergey <sergeyivanyuk89@gmail.com>
2021-09-06 17:56:39 +0300

    add complete project

---
+++ js/main.js
@@ -0,0 +1,27 @@
+﻿$(document).ready(function(){
+
+    $('.gallery-item').magnificPopup({
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+    });
+    
+    $('.gallery-slider').slick({
+        swipeToSlide: true,
+        touchMove: true,
+        swipe:true,
+        touchThreshold: 100,
+        slidesToShow: 3,
+        responsive: [
+            {
+                breakpoint: 750,
+                settings: {
+                    slidesToShow: 2
+                }
+            }
+        ]
+    });
+
+	
+});
